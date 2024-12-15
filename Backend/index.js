c19e7f19b0a6c74593f7b15f0a5ff451ce530da5
ProksFinex <prabhatjnu2@gmail.com>
2024-12-15 11:57:05 +0530

    deploy verison controll

--- Backend/index.js
+++ Backend/index.js
@@ -31,10 +31,10 @@ app.use("/api/pin",pinRoutes);
 
 const __dirname=path.resolve();
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.use(express.static(path.join(__dirname,"/Frontend/dist")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+    res.sendFile(path.join(__dirname,"Frontend","dist","index.html"))
 })
 
 app.listen(port, () => {
